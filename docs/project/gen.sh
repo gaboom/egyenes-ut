105ba0dea1af1255e5486a176f9b75c687bc72bf
Czigola <gabor.czigola@accenture.com>
2023-05-17 22:19:49 +0200

    New project pictures.

--- docs/project/gen.sh
+++ docs/project/gen.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 
-IMAGE_PATH=`dirname ${BASH_SOURCE[0]}`/../../../static/images/projects
+IMAGE_PATH=`dirname ${BASH_SOURCE[0]}`/../../../static/images/projects/T
 
 for file in $IMAGE_PATH/*.jpg; do
   echo -n "Processing $file file... ";
 
   filename=`basename "$file"`
-  year=${filename:4:4}
-  month=${filename:8:2}
-  day=${filename:10:2}
-  hour=${filename:13:2}
-  minute=${filename:15:2}
-  second=${filename:17:2}
+  prefix=${filename:4:5}
+  #year=${filename:4:4}
+  year=2023
+  #month=${filename:8:2}
+  month="05"
+  #day=${filename:10:2}
+  day="01"
+  #hour=${filename:13:2}
+  hour="00"
+  #minute=${filename:15:2}
+  minute="00"
+  #second=${filename:17:2}
+  second="00"
 
-  cat << EOF > PRO_${year}${month}${day}_${hour}${minute}${second}.md
+  cat << EOF > PRO_${year}${month}${day}_${hour}${minute}${second}_${prefix}.md
 ---
-title: "${year}-${month}-${day} ${hour}:${minute}:${second}"
+title: "${year}-${month}-${day} ${hour}:${minute}:${second} ${prefix}"
 date: ${year}-${month}-${day} ${hour}:${minute}:${second}Z
 description: "Projektünk"
-image: "images/projects/${filename}"
-category: "Útépítés"
+image: "images/projects/T/${filename}"
+category: "GINOP"
 ---
 EOF
 
